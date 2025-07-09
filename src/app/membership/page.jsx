bc546b8fd9284979f643a1a8146879aed3afba71
lbarberiscanoni <hllbck7@gmail.com>
2025-07-09 11:00:03 -0700

    fix: mark membership page dynamic

--- src/app/membership/page.jsx
+++ src/app/membership/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useSearchParams, useRouter } from "next/navigation";
