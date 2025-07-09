c2275d6b2fc17603890bfebbc315788daecc54f0
lbarberiscanoni <hllbck7@gmail.com>
2025-07-08 17:20:43 -0700

    trying to solve vercel issue by updating next.config

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js  (rename from .ts if you kept the default)
+const path = require("path");
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
